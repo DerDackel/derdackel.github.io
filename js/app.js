236c3acab8f9fac91085b353c145bed742b1e3e7
Sebastian Jackel <sjackel@uni-koblenz.de>
2015-02-15 12:47:09 +0100

    If I request data, I should make sure it's there

--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@ var app = angular.module('lovecraft-main', []);
 
 var mainCtrl = app.controller("MainCtrl", ['$scope', '$http', function($scope, $http) {
     this.msg = $http.get("/api/v1/test.json").success(function(data) {
-        return data.msg
-    })
+        return data
+    }).error(function(data, status) {
+        console.log("Erroneous response:" + data + ", " + status);
+    });
 }]);
